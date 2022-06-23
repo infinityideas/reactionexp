a2fa88afc1cb8fc48712179d0fa7ccb285ef1f54
Darius Sinha <dsinha@bbns.org>
2022-06-23 14:19:44 -0400

    Intro screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import './styles/app.css';
+import { settings } from './scripts/config';
+import keyboardimg from './content/images/hands-keyboard.jpeg';
 
 function App() {
   return (
-    <div>
-      
+    <div className="container">
+      <h1>Welcome to the Symmetry Reaction Time Experiment</h1>
+      <h3>We appreciate you taking the time to complete this study.</h3><br/><br/>
+      <h3>You will have {settings.time/1000} {settings.time/1000 > 1 ? "seconds" : "second"} to respond whether or not an image is <em>symmetric.</em></h3>
+      <h3>When the image appears on the screen, and if it is symmetric, push down on the spacebar.</h3>
+      <img src={keyboardimg} height="250vh"/><br/>
+      <button>Let's go!</button>
     </div>
   );
 }
---
+++ src/scripts/config.tsx
@@ -0,0 +1,3 @@
+export const settings = {
+    time: 1000
+}
