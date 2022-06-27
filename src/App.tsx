82c77240e9b8a7f4892a8ab43be7846ab68a6a1c
Darius Sinha <dsinha@bbns.org>
2022-06-26 20:26:06 -0400

    Finished experiment

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './styles/app.css';
 import { settings } from './scripts/config';
-import keyboardimg from './content/images/hands-keyboard.jpeg';
+import keyboardimg from './content/images/hands-keyboard.jpg';
 import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="container">
       <h1>Welcome to the Symmetry Reaction Time Experiment</h1>
-      <h3>We appreciate you taking the time to complete this study.</h3><br/><br/>
-      <h3>You will have {settings.time/1000} {settings.time/1000 > 1 ? "seconds" : "second"} to respond whether or not an image is <em>symmetric.</em></h3>
-      <h3>When the image appears on the screen, <br/><br/>if it is <em>symmetric</em>, push down on the <span style={{color: "red"}}>m key</span><br/>if it is <em>not symmetric</em>, push down on the <span style={{color: "red"}}>z key</span></h3>
-      <img src={keyboardimg} height="250vh"/><br/>
-      <Link to="exp"><button>Let's go!</button></Link>
+      <h3>We appreciate you taking the time to complete this study.</h3><h3>We expect this task will take around 10 minutes.</h3><br/><br/>
+      <h3>Each image will be displayed for {settings.time/1000} {settings.time/1000 > 1 ? "seconds" : "second"}. Keep your gaze on the center of the screen.</h3><br/>
+      <h3>When an image appears on the screen, <br/><br/>if it is <em style={{color: "blue"}}>symmetric</em>, press the <span style={{color: "red"}}>m</span> key<br/>if it is <em style={{color: "blue"}}>not symmetric</em>, press the <span style={{color: "red"}}>c</span> key<br/><br/>Try to respond as quickly as possible, although the computer will wait for you to answer before moving on to the next image.<br/>Place one finger from your left hand on the c key and one from your right hand on the m key.</h3>
+      <img src={keyboardimg} height="250vh" alt="keyboard"/><br/><br/>
+      <Link to="exp"><button style={{width: "100px", height: 50, color: "green"}}>Let's go!</button></Link>
     </div>
   );
 }
--- src/Exp.tsx
+++ src/Exp.tsx
@@ -13,6 +13,9 @@ interface ExpState {
     waiting: boolean,
     started: boolean,
     toSubmit: Object,
+    finished: boolean,
+    HITID: string,
+    break: boolean,
 }
 
 class Exp extends React.Component<{}, ExpState> {
@@ -30,21 +33,25 @@ class Exp extends React.Component<{}, ExpState> {
             waited: false,
             waiting: false,
             started: false,
-            toSubmit: {}
+            toSubmit: {},
+            finished: false,
+            HITID: "",
+            break: false
         }
 
         this.timeout = -99;
         this.currentTime = -99;
         this.keyDown = this.keyDown.bind(this);
+        this.afterBreak = this.afterBreak.bind(this);
     }
 
     keyDown(e: any) {
         const timeTaken = Date.now()-this.currentTime;
-        if (e.keyCode == 90 && !this.state.waiting) { // Check for non symmetric
+        if (e.keyCode === 67 && !this.state.waiting && this.state.waited) { // Check for non symmetric
             console.log("NON SYMMETRIC IMAGE "+this.state.currentImage+" | "+(Date.now()-this.currentTime)+"ms taken | "+this.state.images[this.state.order[this.state.currentImage]]);
-        } else if (e.keyCode == 77 && !this.state.waiting) { // Check for symmetric
+        } else if (e.keyCode === 77 && !this.state.waiting && this.state.waited) { // Check for symmetric
             console.log("SYMMETRIC IMAGE "+this.state.currentImage+" | "+(Date.now()-this.currentTime)+"ms taken | "+this.state.images[this.state.order[this.state.currentImage]]);
-        } else if (e.keyCode == 13 && !this.state.waited) {
+        } else if ((e.keyCode === 67 || e.keyCode === 77) && !this.state.waited) {
             this.setState({
                 waiting: true
             })
@@ -54,19 +61,24 @@ class Exp extends React.Component<{}, ExpState> {
                     waiting: false,
                     started: true
                 })
-            }, 3000)
+            }, 3000);
+            return
         }
 
-        if ((e.keyCode == 90) || (e.keyCode == 77) && !this.state.waiting) {
+        if (((e.keyCode === 67) || (e.keyCode === 77)) && this.state.break) {
+            this.afterBreak();
+        }
+
+        if (((e.keyCode === 67) || (e.keyCode === 77)) && !this.state.waiting && this.state.waited && !this.state.break) {
             let currentSubmit: any = this.state.toSubmit;
             currentSubmit[this.state.currentImage.toString()] = {
                 time: timeTaken,
                 imageNumber: this.state.order[this.state.currentImage],
                 imageURL: this.state.images[this.state.order[this.state.currentImage]],
-                answer: e.keyCode == 90 ? "NS" : "S"
+                answer: e.keyCode === 67 ? "NS" : "S"
             }
 
-            if (this.state.currentImage+1 == this.state.images.length) {
+            if (this.state.currentImage+1 === this.state.images.length) {
                 console.log(currentSubmit);
                 axios.post(settings.flaskServer+"data", JSON.stringify(currentSubmit), {
                     headers: {
@@ -74,11 +86,20 @@ class Exp extends React.Component<{}, ExpState> {
                         'x-api-key': settings.KORAPIKey
                     }
                 }).then((resp: any) => {
-                    console.log(resp);
+                    this.setState({
+                        finished: true,
+                        HITID: resp["data"]["id"]
+                    })
                 })
                 return;
+            } else if ((this.state.currentImage % 50 == 0) && this.state.currentImage != 0) {
+                this.setState({
+                    break: true,
+                    toSubmit: currentSubmit
+                });
+                return;
             }
-            if (this.timeout != -99) {
+            if (this.timeout !== -99) {
                 clearTimeout(this.timeout);
             }
             this.setState({
@@ -128,7 +149,46 @@ class Exp extends React.Component<{}, ExpState> {
         }
     }
 
+    afterBreak() {
+        if (this.timeout !== -99) {
+            clearTimeout(this.timeout);
+        }
+        this.setState({
+            waiting: true,
+            break: false
+        });
+        setTimeout(() => {
+            this.setState({
+                waiting: false,
+                showingImage: true,
+                currentImage: this.state.currentImage+1,
+            })
+        }, 1000)
+    }
+
     render() {
+        if (this.state.break) {
+            return (
+                <div style={{textAlign: "center", fontFamily: "sans-serif"}}>
+                    <h1>Take a break!</h1>
+                    <h3>Get up, walk around, have some water and come back when you're ready.</h3><br/>
+                    <h3>Remember: you have one hour from when you started the task to finish it.</h3><br/>
+                    <h3><strong>DO NOT</strong> close this tab! You will lose your progress.</h3>
+                    <br/>
+                    <h3>Press <span style={{color: "red"}}>m</span> or <span style={{color: "red"}}>c</span> to keep going</h3>
+                </div>
+            )
+        }
+        if (this.state.finished) {
+            return (
+                <div style={{textAlign: "center", fontFamily: "sans-serif"}}>
+                    <h1>Thank you for completing the study!</h1>
+                    <h3>Please enter the following code into MTURK as your completion id: <span style={{color: "red"}}>{this.state.HITID}</span></h3>
+                    <br/><br/>
+                    <h4>Any questions? Please email psinha@mit.edu. You will need to send your completion id for us to assist with any issues.</h4>
+                </div>
+            )
+        }
         if (!this.state.ready) {
             return (
                 <div style={{textAlign: "center", fontFamily: "sans-serif"}}>
@@ -150,14 +210,14 @@ class Exp extends React.Component<{}, ExpState> {
         }
         if (!this.state.waited) {
             return (
-                <div style={{textAlign: "center", fontFamily: "sans-serif"}}><h1>Press return to start</h1></div>
+                <div style={{textAlign: "center", fontFamily: "sans-serif"}}><h1>Press <span style={{color: "red"}}>m</span> or <span style={{color: "red"}}>c</span> to start</h1></div>
             )
         }
         if (this.state.showingImage) {
             this.currentTime = Date.now()
             return (
-                <div style={{textAlign: "center"}}>
-                    <img src={this.state.images[this.state.order[this.state.currentImage]]} />
+                <div style={{textAlign: "center", width: "100%", paddingTop: window.innerHeight/2-window.innerWidth/10}}>
+                    <img src={this.state.images[this.state.order[this.state.currentImage]]} width="100%" alt=""/>
                 </div>
             )
         }
